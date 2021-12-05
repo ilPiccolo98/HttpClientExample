771ac3e8bc8f37270e38316791aa8835f58baf8c
giuseppe <peppe.piccolo@protonmail.com>
2021-12-05 21:04:04 +0100

    project completed

---
+++ HttpClientNetworking/Downloader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace HttpClientNetworking
+{
+    internal class Downloader
+    {
+        public static async Task DownloadWebPage()
+        {
+            using HttpClient httpClient = new();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(url);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                byte[] data = await responseMessage.Content.ReadAsByteArrayAsync();
+                using FileStream fileStream = File.Create(fileName);
+                await fileStream.WriteAsync(data);
+            }
+        }
+
+        private const string url = "https://16bpp.net/";
+        private const string fileName = "index.html";
+    }
+}
---
+++ HttpClientNetworking/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Threading.Tasks;
+
+namespace HttpClientNetworking
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task task = Downloader.DownloadWebPage();
+            task.GetAwaiter().GetResult();
+        }
+    }
+}
+
+
